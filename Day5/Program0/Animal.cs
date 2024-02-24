7918448b09ab0af78a72f1281886c137770b0f86
shukrina <68145760+shukrina@users.noreply.github.com>
2024-02-24 16:06:28 +0545

    Add files via upload

---
+++ Day5/Program0/Animal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program0
+{
+    //program to illustrate how value of an object is passed to a method
+    public class Animal
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+
+
+    }
+}
