dcb7f263f73fdde56684c0f64e6d81aa0b75d84e
shukrina <68145760+shukrina@users.noreply.github.com>
2024-02-24 16:16:04 +0545

    Edited parameters

--- Day5/Program7/Program.cs
+++ Day5/Program7/Program.cs
@@ -13,13 +13,13 @@ namespace Program7
         {
             Animal animal = new Animal("rabbit", "herbivorous", "domestic");
             Console.WriteLine(animal.Name +" is a " + animal.Description + " and a " + animal.Type+" animal");
-            animal.WhereItLives(animal.Type);
-            animal.WhatItEats (animal.Description);
+            animal.WhereItLives();
+            animal.WhatItEats ();
             Console.WriteLine();
 
             WildAnimal animal2 = new WildAnimal("Leopard", "carnivorous", "wild");
             Console.WriteLine(animal2.Name+ " is a "+ animal2.Description+ " and a "+ animal2.Type+" animal");
-            animal2.WhereItLives(animal2.Type);
+            animal2.WhereItLives();
             Console.ReadKey();
         }
     }
