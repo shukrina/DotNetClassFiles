e115b75a19dbb378fb2f8572358928a34434db89
shukrina <68145760+shukrina@users.noreply.github.com>
2024-02-25 18:23:33 +0545

    Add files via upload

---
+++ Day6/Program9/Animal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program9
+{
+    //interface basic program
+    public interface Animal
+    {
+        string Name { get; set; }
+        string Type { get; set; }
+
+        void WhereItLives();
+    }
+    internal class WildAnimal: Animal
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public void WhereItLives()
+        {
+            if (Type == "wild")
+                Console.WriteLine(Name+ " lives in jungle.");
+            else
+                Console.WriteLine(Name+ " lives at home.");
+        }
+    }
+}
---
+++ Day6/Program9/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program9
+{
+    //interface basic program
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new WildAnimal();
+            animal.Name = "Lion";
+            animal.Type = "wild";
+            animal.WhereItLives();
+            Console.ReadKey();
+        }
+    }
+}
