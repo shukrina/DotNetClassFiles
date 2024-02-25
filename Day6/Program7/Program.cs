78b14605fc2a2891483b560359f708fae707f6f0
shukrina <68145760+shukrina@users.noreply.github.com>
2024-02-25 17:43:14 +0545

    Add files via upload

---
+++ Day6/Program7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program7
+{
+    //general case for exception handling
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a = Convert.ToInt32("111111111111111111111");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Day6/Program8/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program8
+{
+    //using finally for garnai parney kaam
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int num = Convert.ToInt32("1313rs");
+                int divisor = 0;
+                int result = 17 / divisor;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Input was not in correct format.");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("You attempted to divide by zero.");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("You did what needs to be done.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
