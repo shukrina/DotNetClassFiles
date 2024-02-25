f19323ef4da4ce6fcfc086bbe1a50c635b2ed14d
shukrina <68145760+shukrina@users.noreply.github.com>
2024-02-25 17:23:50 +0545

    Add files via upload

---
+++ Day6/Program4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program4
+{
+    //format exception handling
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a ;
+            try
+            {
+                a = Convert.ToInt32("Ram");
+            }
+            catch(FormatException e)  //catch(FormatException) matra rakhey ni chalxa
+            {
+                //Console.WriteLine(e.Message);
+                Console.WriteLine("There is a format exception");
+            }
+            Console.ReadKey();
+        }
+    }
+}
